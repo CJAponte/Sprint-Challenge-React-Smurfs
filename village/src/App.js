cd0ecb35d222f4e69b763415bea464a2593b5257
unknown <cesar.aponte.1996.vazquez@gmail.com>
2019-06-14 12:42:20 -0400

    almost completed. Need to add the post and delete functionality and nav bar. Currently unable to add new smurf.

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import { Route, NavLink } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -11,14 +12,31 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount = () => {
+    axios.get('http://localhost:3333/smurfs')
+      .then(response => {
+        if (typeof response.data.message === 'string') {
+          Promise.reject("Error: smurfs are missing!")
+        }
+        this.setState({ smurfs: response.data })
+      })
+      .catch(err => console.log(err))
+  }
+
+
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+
+
+        <Route path='/smurf-form' render={(props) => <SmurfForm {...props} addNewSmurf={this.addNewSmurf} />} />
+        <Route path='/' render={(props) => <Smurfs {...props} smurfs={this.state.smurfs} delete={this.deleteSmurf} />} />
+
       </div>
     );
   }
