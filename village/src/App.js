580830ba3faed1059fe2799ce4e3f22c100323c3
unknown <cesar.aponte.1996.vazquez@gmail.com>
2019-06-14 12:55:17 -0400

    Adding Smurfs completed. Nav links completed. Working on smurf delete.

--- village/src/App.js
+++ village/src/App.js
@@ -24,7 +24,27 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
+  addNewSmurf = (obj) => {
+    axios.post('http://localhost:3333/smurfs', obj)
+      .then(response => {
+        this.setState({
+          smurfs: response.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
 
+  deleteSmurf = (id) => {
+    return () => {
+      axios.delete(`http://localhost:3333/smurfs/${id}`)
+        .then(response => {
+          this.setState({
+            smurfs: response.data
+          })
+        })
+        .catch(err => console.log(err))
+    }
+  }
 
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
@@ -32,7 +52,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/smurf-form'>Add New Smurf</NavLink>
+        </nav>
 
         <Route path='/smurf-form' render={(props) => <SmurfForm {...props} addNewSmurf={this.addNewSmurf} />} />
         <Route path='/' render={(props) => <Smurfs {...props} smurfs={this.state.smurfs} delete={this.deleteSmurf} />} />
